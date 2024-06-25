0ff2db72d7ecff270065d594d967e91dcf0035b4
BadrRibzat <badrribzat@gmail.com>
2024-06-25 18:36:29 +0000

    0x03-queuing_system_in_js

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,11 +1,12 @@
-import redis from 'redis';
+import { createClient } from 'redis';
 
-// we should creates a new client and this is how :
-const client = redis.createClient(); 
-// redis.createClient() by defaulr will use 127.0.0.1 and port 6379
+// Connect to the Redis server running locally.
+const client = createClient();
 
-// whenever client.on(çonnect') see whether we successfully connected to the redis-server by listening 
-client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
 
-// we should be able to listen for the error event if we failed to connect to the redis-server
-client.on('error', (err) => console.error(`Redis client not connected to the server: ${err.message}`));
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error.message}`);
+});
--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,30 +1,27 @@
-// To import the library
-import redis from 'redis';
+import { createClient, print } from 'redis';
 
-// this creates a new client
-const client = redis.createClient(); 
-// By default redis.createClient() will use 127.0.0.1 and port 6379
+// Connect to the Redis server running locally.
+const client = createClient();
 
-// listen for the connect event to see whether we successfully connected to the redis-server
-client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
 
-// listen for the error event tocheck if we failed to connect to the redis-server
-client.on('error', (err) => console.error(`Redis client not connected to the server: ${err.message}`));
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error.message}`);
+});
 
-function setNewSchool(schoolName, value) {
-    // redis.print prints “Reply: OK” to the console saying that redis saved the value
-    client.set(schoolName, value, redis.print);
-}
+// Set in Redis the value for the key `schoolName`.
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, print);
+};
 
-function displaySchoolValue(schoolName) {
-    client.get(schoolName, (err, result) => {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
-    console.log(result);
-    });
-}
+// Log the value of the key `schoolName` into the console.
+const displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (error, reply) => {
+    console.log(reply);
+  });
+};
 
 displaySchoolValue('Holberton');
 setNewSchool('HolbertonSanFrancisco', '100');
